e46941585cfec63520e485d40076618cd6fc5bde
andgursky <andgursky@gmail.com>
2017-03-11 23:51:24 +0200

    class Email - was emplemented
    1. Simple email sending process was added.
    2. Some changes in check.rb was added

--- lib/status_checker/check.rb
+++ lib/status_checker/check.rb
@@ -1,16 +1,18 @@
 require "status_checker/timer"
+require "status_checker/email"
 require "net/https"
 require "uri"
 
 module StatusChecker
   class Check
-    def initialize(email="andgursky@gmail.com", per=10,
-                   domain=["https://pokupon.ua", "https://partner.pokupon.ua"])
+    def initialize(email=["andgursky@gmail.com"], per=10,
+                   domain=["https://ukr.net", "https://yandex.ru"])
       # initialize all instanse variables
       @email    = email
       @delay    = per
       @domain   = domain
       @timer    = StatusChecker::Timer.new(@delay)
+      @resp_stack = Hash.new
     end
 
     def start
@@ -19,8 +21,11 @@ module StatusChecker
       @timer.start do
         @domain.each do |dom|
           threads << Thread.new(dom) do |url|
-            response = check_http_status_of(url)
-            email.send(response) unless response.code == 200
+            response = check_http_status_of url
+            if response.code != 200.to_s && !recurrent_code?(url, response.code)
+              StatusChecker::Email.new(@email).send(url, response)
+              add_code_to_resp_stack(response.code, url)
+            end
           end
         end
         threads.each { |thr| thr.join }
@@ -40,6 +45,17 @@ module StatusChecker
       http.use_ssl = true if uri.scheme == "https"
       request = Net::HTTP::Get.new(uri.request_uri)
       http.request(request)
+    end
+
+    def recurrent_code?(url, resp_code)
+      @resp_stack.each_pair do |key, val|
+        return true if key==resp_code && val==url
+      end
+      return false
+    end
+
+    def add_code_to_resp_stack(code, url)
+      @resp_stack.merge!("#{code}" => url)
     end
   end
 end
---
+++ lib/status_checker/email.rb
@@ -0,0 +1,42 @@
+require 'net/smtp'
+require 'pry'
+
+module StatusChecker
+  class Email
+    def initialize(receivers)
+      @receivers = receivers
+      @addres = 'smtp.mail.ru'
+      @port = 465
+      @domain = 'mail.ru'
+      @authentication = :plain
+      @user_name = 'technical_root@mail.ru'
+      @password = 'qwerty123456'
+      @smtp = Net::SMTP.new(@addres, @port)
+      @smtp.enable_tls
+    end
+
+    def send(url, response)
+      begin
+        @smtp.start(@domain, @user_name, @password, @authentication)
+        @receivers.each do |email|
+          @smtp.send_message message(url, response, email), @user_name, email
+        end
+      ensure
+        @smtp.finish
+      end
+    end
+
+    private
+
+    def message(url, response, email)
+      "From:<#{@user_name}>\n" \
+      "To:<#{email}>\n" \
+      "MIME-Version: 1.0\n" \
+      "Content-type: text/html\n" \
+      "Subject: Attention!!!\n" \
+      "<h1 style='color:red'><b>!!!ATTENTION!!!</b></h1>\n" \
+      "<p>The host #{url} #{response.message} and has " \
+      "<b>code: #{response.code}</b></p>"
+    end
+  end
+end
